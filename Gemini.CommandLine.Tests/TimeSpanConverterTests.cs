db45406133176127f7660d6c747d1ba26a18ee20
Dave Van den Eynde <DaveVdE@users.noreply.github.com>
2011-03-25 14:30:45 +0100

    Added a new typeconverter for timespans that supports a user friendly conversion.

---
+++ Gemini.CommandLine.Tests/TimeSpanConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gemini.CommandLine.Tests
+{
+    [TestClass]
+    public class TimeSpanConverterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const string input = "7d 12h";
+            var output = new MyTimeSpanConverter().ConvertFrom(input);
+            var compare = TimeSpan.FromDays(7) + TimeSpan.FromHours(12);
+
+            Assert.AreEqual(output, compare);
+        }
+    }
+}
---
+++ Gemini.CommandLine/MyTimeSpanConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Gemini.CommandLine
+{
+    public class MyTimeSpanConverter : TypeConverter
+    {
+        private const string FindGroups = @"([\d.,]+)([smhdwSMHDW])";
+
+        private static TimeSpan InternalConvert(CultureInfo culture, string input)
+        {
+            Regex findGroupsExpression = new Regex(FindGroups);
+            MatchCollection matches = findGroupsExpression.Matches(input);
+            TimeSpan result = TimeSpan.Zero;
+
+            foreach (Match match in matches)
+            {
+                double number = Convert.ToDouble(match.Groups[1].Value, culture);
+                switch (match.Groups[2].Value)
+                {
+                    case "s":
+                    case "S":
+                        result += TimeSpan.FromSeconds(number);
+                        break;
+                    case "m":
+                    case "M":
+                        result += TimeSpan.FromMinutes(number);
+                        break;
+                    case "h":
+                    case "H":
+                        result += TimeSpan.FromHours(number);
+                        break;
+                    case "d":
+                    case "D":
+                        result += TimeSpan.FromDays(number);
+                        break;
+                    case "w":
+                    case "W":
+                        result += TimeSpan.FromDays(number*7);
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            return InternalConvert(culture, Convert.ToString(value));
+        }
+    }
+}
