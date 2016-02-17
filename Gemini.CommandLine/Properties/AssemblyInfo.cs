5ae76d8d92fe32654ce2972d3e931bc00985232c
Dave Van den Eynde <dave@groovbird.com>
2016-02-17 19:49:38 +0100

    Adding nuget spec and updating version number.

--- Gemini.CommandLine/Properties/AssemblyInfo.cs
+++ Gemini.CommandLine/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gemini.CommandLine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Gemini.CommandLine lets you create commandline apps quickly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Dave Van den Eynde")]
 [assembly: AssemblyProduct("Gemini.CommandLine")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Dave Van den Eynde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
